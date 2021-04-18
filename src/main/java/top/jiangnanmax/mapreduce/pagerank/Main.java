914674f2aedeb19a0956f82b957b87668fb68c7c
JiangNanMax <1321064814@qq.com>
2021-04-19 00:27:54 +0800

    test branch

---
+++ src/main/java/top/jiangnanmax/mapreduce/pagerank/Main.java
@@ -0,0 +1,11 @@
+package top.jiangnanmax.mapreduce.pagerank;
+
+/**
+ * @author jiangnanmax
+ * @email jiangnanmax@gmail.com
+ * @description Main
+ * @date 2021/4/19
+ **/
+
+public class Main {
+}
---
+++ src/main/java/top/jiangnanmax/mapreduce/util/Util.java
@@ -0,0 +1,11 @@
+package top.jiangnanmax.mapreduce.util;
+
+/**
+ * @author jiangnanmax
+ * @email jiangnanmax@gmail.com
+ * @description Util
+ * @date 2021/4/19
+ **/
+
+public class Util {
+}
