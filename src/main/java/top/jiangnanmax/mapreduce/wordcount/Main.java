7965748f0a960faeba2178ef70d4a60aedc2732f
JiangNanMax <1321064814@qq.com>
2021-04-19 00:23:55 +0800

    test branch

---
+++ src/main/java/top/jiangnanmax/mapreduce/wordcount/Main.java
@@ -0,0 +1,11 @@
+package top.jiangnanmax.mapreduce.wordcount;
+
+/**
+ * @author jiangnanmax
+ * @email jiangnanmax@gmail.com
+ * @description Main
+ * @date 2021/4/19
+ **/
+
+public class Main {
+}
